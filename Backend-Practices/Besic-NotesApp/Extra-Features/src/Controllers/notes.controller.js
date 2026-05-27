da3613a101e73bd5660f45c7c32faf6d58695cec
Swarup Das <swarupdas2046@gmail.com>
2026-05-27 11:30:41 +0530

    add some extra features

--- Backend-Practices/Besic-NotesApp/Extra-Features/src/Controllers/notes.controller.js
+++ Backend-Practices/Besic-NotesApp/Extra-Features/src/Controllers/notes.controller.js
@@ -54,3 +54,77 @@ export const ViewALLNotes = async(req,res)=>{
         AllNotes,
     })
 }
+
+// Update Existing Notes 
+
+export const UpdateNotes = async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log("id Coming from frontend---->", id);
+
+    const { title, description, priority, category } = req.body;
+
+    // validate required fields are filled or not
+    if (!title || !description || !priority) {
+      return res.status(400).json({
+        massage: "Title , description & priority fields are mandatory",
+      });
+    }
+
+    // Check and updated in this id  our database
+
+ const UpdatedNotes = await NotesModel.findByIdAndUpdate(
+      id,
+      {
+        title,
+        description,
+        priority,
+        category,
+      },
+      {
+        new: true,
+      },
+    );
+
+    // Check if note existed
+    if (!UpdatedNotes) {
+      return res.status(404).json({
+        massage: "Notes are Not Existed in this Id",
+      });
+    }
+    
+    return res.status(200).json({
+      massage: "Notes Update Successfully",
+      UpdatedNotes,
+    });
+
+
+  } catch (error) {
+    return res.status(500).json({
+      massage: "Internal Server Error",
+      error,
+    });
+  }
+}
+
+export const DeleteNotes =  async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const IsExisted = await NotesModel.findById(id);
+    if (!IsExisted) {
+      return res.status("Notes are not Found in this Id");
+    }
+
+    await NotesModel.findByIdAndDelete(id);
+
+    return res.status(200).json({
+      Massage: "Notes DELETED SUccessFully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      Massage: "Internal Server Error",
+      error,
+    });
+  }
+}
--- Backend-Practices/Besic-NotesApp/Extra-Features/src/Routes/notes.route.js
+++ Backend-Practices/Besic-NotesApp/Extra-Features/src/Routes/notes.route.js
@@ -1,16 +1,24 @@
-import express from 'express'
-import { CreateNotes, ViewALLNotes } from '../Controllers/notes.controller.js'
-import { NotesModel } from '../Models/notes.models.js'
+import express from "express";
+import {
+  CreateNotes,
+  DeleteNotes,
+  UpdateNotes,
+  ViewALLNotes,
+} from "../Controllers/notes.controller.js";
+import { NotesModel } from "../Models/notes.models.js";
 
+const Routes = express.Router();
 
-const Routes = express.Router()
-
-// create notes api 
-Routes.post("/Notes/create",CreateNotes)
+// create notes api
+Routes.post("/Notes/create", CreateNotes);
 
 // view notes api
-Routes.get("/Notes/view",ViewALLNotes)
+Routes.get("/Notes/view", ViewALLNotes);
 
+// Update Notes Api
+Routes.put("/Notes/update/:id", UpdateNotes);
 
+// Delete Notes Api
+Routes.delete("/Notes/delete/:id",DeleteNotes);
 
-export default Routes
+export default Routes;
