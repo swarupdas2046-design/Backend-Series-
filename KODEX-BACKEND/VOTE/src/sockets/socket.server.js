23684e40cec9940b18949cf5a071541f2b706570
Swarup Das <swarupdas2046@gmail.com>
2026-06-09 19:30:11 +0530

    VOTE

--- KODEX-BACKEND/VOTE/src/sockets/socket.server.js
+++ KODEX-BACKEND/VOTE/src/sockets/socket.server.js
@@ -1,35 +1,47 @@
-import { Server } from 'socket.io'
+import { Server } from "socket.io"
 
-const voteCount = {
-    yes: 0,
-    no: 0
-}
+const voteCounts = {}
 
+const sockets = []
 
 const initSocket = (httpServer)=>{
-    
     const io = new Server(httpServer)
 
-    io.on('connection', (socket)=>{
-        console.log('a user connected 🥰')
+    io.on("connect",(socket)=>{
+        console.log("a User Connected 🥰");
 
-        socket.on('vote_yes', ()=>{
-            voteCount.yes+=1
-            io.emit('Vote_Update', voteCount)
+        const {room} = socket.handshake.query
+        console.log(`User JOin Room := ${room}`);
+
+        socket.join(room)
+
+            if (!voteCounts[ room ]) {
+            voteCounts[ room ] = { yes: 0, no: 0 };
+        }
+        
+        socket.on("vote_yes",()=>{
+            if (sockets.includes(socket.id)) {
+                return;
+            }
+            voteCounts[ room ].yes += 1;
+            io.to(room).emit("Votes_Updated",voteCounts[ room ])
+            sockets.push(socket.id)
         })
 
-        socket.on('vote_no', ()=>{
-            voteCount.no+=1
-            io.emit('Vote_Update', voteCount)
+        socket.on("vote_no",()=>{
+            if (sockets.includes(socket.id)) {
+                return;
+            }
+            voteCounts[ room ].no += 1;
+            io.to(room).emit("Votes_Updated",voteCounts[ room ])
+            sockets.push(socket.id)
         })
 
-
-    
-        socket.on('disconnect', ()=>{
-            console.log('user disconnected 😭')
+        socket.on("disconnect",()=>{
+            console.log("a User Disconnected 😭");
         })
     })
-    
 }
+
 
 export default initSocket
