92905d347246da3406e64e2437ee39491757e248
Swarup Das <swarupdas2046@gmail.com>
2026-08-09 19:38:03 +0530

    feat: update email subject and body in sendmail endpoint

--- KODEX-BACKEND/day18-SMTP-Nodemailer/src/app.js
+++ KODEX-BACKEND/day18-SMTP-Nodemailer/src/app.js
@@ -8,8 +8,8 @@ app.use(express.json());
 app.get("/sendmail", async (req, res) => {
   await SendEmail(
     "daspriti1021@gmail.com",
-    "KIU re MADARCHOD",
-    "MERE bacheee chus le...........",
+    "hey greeting mail",
+    "Hey,Just wanted to see how you're doing. Let's catch up soon over the phone or grab some food.Hope everything is good with you!Best,swarup Das",
   );
 
   return res.send("Chala gaya mail Client ke pas");
