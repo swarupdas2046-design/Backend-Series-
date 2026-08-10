2504e911f7cf15f5648d832e91ea41d4eac18f7a
Swarup Das <swarupdas2046@gmail.com>
2026-08-10 21:11:47 +0530

    feat: initialize forgot password feature with Express server setup
    - Created package.json for project dependencies and scripts
    - Added server.js to start the Express application
    - Implemented app.js to configure Express middleware for JSON parsing

---
+++ KODEX-BACKEND/Day19-forgot-password/server.js
@@ -0,0 +1,9 @@
+import "dotenv/config"
+import app from "./src/app.js"
+
+const port = Number(process.env.PORT) || 5000
+
+
+app.listen(port, () => {
+    console.log(`Server running successfully on port`, port)
+})
