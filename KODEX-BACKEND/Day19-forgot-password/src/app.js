6fc003ba20631081c2626389b0b361af7b4fe03c
Swarup Das <swarupdas2046@gmail.com>
2026-08-12 22:43:53 +0530

    feat: set up EJS view engine and update root route to render index page

--- KODEX-BACKEND/Day19-forgot-password/src/app.js
+++ KODEX-BACKEND/Day19-forgot-password/src/app.js
@@ -3,19 +3,29 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import authRoutes from './routes/auth.routes.js'
 import { protect } from './middlewares/auth.middleware.js'
+import path from 'path'
+import { fileURLToPath } from 'url'
+// import { name } from 'ejs'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-app.get('/', protect, (req, res) => {
-    res.json({
-        success: true,
-        message: 'Node Express auth API is running'
-    })
+console.log(__dirname);
+
+
+app.get('/',(req, res) => {
+    res.render("index")
 })
 
 app.use('/api/auth', authRoutes)
